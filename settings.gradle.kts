3fb086115d02db4873ecd5586bc99ebbbba42f6e
ege2509 <ekurtulus@sabanciuniv.edu>
2025-05-05 21:25:18 +0300

    Project created

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ENS492 Frontend"
+include(":app")
